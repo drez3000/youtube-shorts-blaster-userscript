2447998b45a384a94e87ea1b64c9e48e875acba1
drez3000 <121506690+drez3000@users.noreply.github.com>
2024-10-10 13:18:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# YouTube Shorts Blaster UserScript
+
+## Overview
+
+A userscript to automatically detect and remove any YouTube page element containing shorts outside of the "/shorts" page itself.
+
+## Installation
+
+### 1. Ensure your browser has a userscript manager
+
+- **Google Chrome**: Install [Violentmonkey](https://chrome.google.com/webstore/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag) or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+- **Mozilla Firefox**: Install [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/), or [Violentmonkey](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/), or [Tampermonkey](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/).
+- **Opera**: Install [this](https://addons.opera.com/en/extensions/details/install-chrome-extensions/) extension first, then you can install Violentmonkey or Tampermonkey from the Chrome extension store.
+- **Microsoft Edge**: Install [Violentmonkey](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao) or [Tampermonkey](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd) from the Edge Add-On Repository.
+
+### 2. Click [here](https://raw.githubusercontent.com/drez3000/youtube-shorts-blaster-userscript/main/src/userscript.user.js)
+
+### 3. Confirm your intention to install the userscript
+
+## Disclaimer
+
+Use this userscript at your own risk. The authors are not responsible for any potential issues that may arise from the use of this software, including but not limited to the removal of important content or impacts on user experience.
+
+## License
+
+This is an open source project released under MIT license. See `LICENSE` file for details.
---
+++ src/userscript.user.js
@@ -0,0 +1,118 @@
+// ==UserScript==
+// @name         YouTube Shorts Blaster
+// @description  A userscript to automatically detect and remove YouTube page elements containing shorts outside of the "/shorts" page itself.
+// @namespace    drez3000
+// @author       drez3000
+// @copyright    2024, drez3000 (https://github.com/drez3000)
+// @license      MIT
+// @match        *://*.youtube.com/*
+// @exclude      *://accounts.youtube.com/*
+// @grant        none
+// @version      0.1.0
+// @updateURL    https://raw.githubusercontent.com/drez3000/youtube-shorts-blaster-userscript/main/src/userscript.user.js
+// @downloadURL  https://raw.githubusercontent.com/drez3000/youtube-shorts-blaster-userscript/main/src/userscript.user.js
+// ==/UserScript==
+
+; (() => {
+	'use strict'
+
+	const MAX_TRIGGERS = 2
+	const ATTEMPTS = 8
+	const MAX_CHECK_DELAY_MS = 200
+
+	function oncePageLoaded(callback) {
+		if (document.readyState !== 'loading') {
+			// Document is already ready, call the callback immediately
+			callback()
+		} else {
+			// Document is not ready yet, wait for the DOMContentLoaded event
+			document.addEventListener('DOMContentLoaded', callback)
+		}
+	}
+
+	function flatNodesOf(node, { minDepth = 0, maxDepth = Number.POSITIVE_INFINITY, includeShadowRoot = true } = {}) {
+		const nodes = []
+		const stack = [{ node, depth: 0 }]
+		while (stack.length > 0) {
+			const { node: currentNode, depth } = stack.pop()
+
+			if (depth >= minDepth && depth <= maxDepth) {
+				nodes.push({ node: currentNode, depth })
+			}
+
+			// Add children to the stack with increased depth
+			for (let i = currentNode.childNodes.length - 1; i >= 0; i--) {
+				stack.push({ node: currentNode.childNodes[i], depth: depth + 1 })
+			}
+			if (includeShadowRoot && currentNode.shadowRoot) {
+				stack.push({ node: currentNode.shadowRoot, depth: depth + 1 })
+			}
+		}
+		return nodes.sort((a, b) => a.depth - b.depth).map((item) => item.node)
+	}
+
+	function isMain(node) {
+		const tagName = node?.tagName || ''
+		return tagName.match(/^(html|main|body|content|article)$/i)
+	}
+
+	function isShortsElement(node) {
+		const tagName = node?.tagName || ''
+		const headers = [...node.querySelectorAll(`h1,h2,h3,h4,h5,h6`)]
+		return (
+			tagName.match(/reel/i)
+			|| (node?.attributes && node.attributes['is-shorts'] != undefined)
+			|| (headers.length === 1 && headers.at(0).innerText.match(/^shorts$/i))
+		)
+	}
+
+	function removeYoutubeShorts() {
+		return flatNodesOf(document)
+			.filter((node) => (
+				typeof node?.querySelectorAll === 'function'
+				&& node?.attributes != undefined
+				&& !isMain(node)
+				&& isShortsElement(node)
+			))
+			.map((node) => {
+				node.remove && node.remove()
+				return node
+			})
+	}
+
+	function enqueue(a = 0, triggered = 0) {
+		if (
+			document.location.href.match(/youtube\..*\/shorts/i)
+			|| document.location.href.match(/youtube\..*\/history/i)
+			|| document.location.href.match(/youtube\..*\/playlist/i)
+			|| document.location.href.match(/youtube\..*\/account/i)
+		) {
+			return
+		}
+		const ms = Math.max(0, Math.min(10 ** (a - 1), MAX_CHECK_DELAY_MS))
+		setTimeout(() => {
+			const matches = removeYoutubeShorts()
+			if (matches.length) {
+				console.info(`[YOUTUBE SHORTS BLASTER] Removed ${matches.length} elements:`, matches)
+			}
+			triggered += matches.length
+			if (triggered < MAX_TRIGGERS && a < ATTEMPTS) {
+				enqueue(a + 1, triggered)
+			}
+		}, ms)
+	}
+
+	function main() {
+		let loc = document?.location?.href
+		setInterval(() => {
+			const _loc = document?.location?.href
+			if (_loc != loc) {
+				loc = _loc
+				enqueue()
+			}
+		}, 100)
+		oncePageLoaded(enqueue)
+	}
+
+	main()
+})()
